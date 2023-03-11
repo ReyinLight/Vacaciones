61c033fbc4a8dee228f34df0615aaf667c3b35f2
Luis Reyes <ReyinLight@gmail.com>
2023-03-11 01:25:23 -0600

    Refactor and tests update

--- src/main/java/org/example/Empleado.java
+++ src/main/java/org/example/Empleado.java
@@ -18,80 +18,66 @@ public class Empleado {
     @Getter @Setter
     int vacacionesTomadas;
 
-
-    public String toString(){
-
-        DecimalFormat f = new DecimalFormat("##.00");
-        double diasVacaciones = calcularVacacionesProporcionalesNUEVAley();
-
-        return "Nombre: " + nombre +
-                "\nFecha de ingreso: " + fechaIngreso.toString() +
-                "\nVacaciones: " + f.format(diasVacaciones) +
-                "\nVacaciones tomadas: " + vacacionesTomadas +
-                "\nVacaciones restantes: " + f.format(diasVacaciones-vacacionesTomadas);
+    public double calcularVacaciones(){
+        return Double.parseDouble(new DecimalFormat("#.##").format(calcularUsandoViejaLey() + calcularDesdeNuevaLey()));
     }
 
-
-
     // Calcula los dias de vacaciones por años completos y el proporcional al ultimo año con la NUEVA ley
-    public double calcularVacacionesProporcionalesNUEVAley() {
+    public double calcularDesdeNuevaLey() {
 
-        double añosTotales =Period.between(fechaIngreso, LocalDate.now()).getYears();
+        double añosTotales = Period.between(fechaIngreso, LocalDate.now()).getYears();
         double añosViejaLey = getAñosHastaAniversario2022();
-
-        double total = calcularVacacionesViejaLey();
+        double totalVacaciones = 0; // calcularUsandoViejaLey();
         int diasVacas = 12;
         int i;
 
-        // Se recorrerán los años para evaluar los dias de vacaciones correspondientes
-        for (i = 1; i <= 5 && i <= añosTotales; i++, diasVacas += 2)
-            if(i > añosViejaLey) // Si los años actuales ya superaron a los de la nueva ley, entonces se agregan los dias de vacaciones nuevos
-                total += diasVacas;
-
-        for (; i < añosTotales; i++)
-        {
+        // Se recorrerán los años para evaluar los dias de vacaciones por años completos
+        for (i = 0; i < 5 && i < añosTotales; i++){
             if(i >= añosViejaLey) // Si los años actuales ya superaron a los de la nueva ley, entonces se agregan los dias de vacaciones nuevos
-                total += diasVacas;
+                totalVacaciones += diasVacas;
+            diasVacas += 2;
+        }
+
+        for (i = i + 1; i < añosTotales; i++){
+            if(i >= añosViejaLey) // Si los años actuales ya superaron a los de la nueva ley, entonces se agregan los dias de vacaciones nuevos
+                totalVacaciones += diasVacas;
 
             if (i % 5 == 0) // Cada 5 años se incrementan los dias de vacaciones en 2
                 diasVacas += 2;
         }
 
+        // Calcular fraccion del ultimo año
         // Si en el año actual es multiplo de 5 entonces corresponde un incremento de dias de vacaciones
         if (i % 5 == 0)
             diasVacas += 2;
 
         // Se calculan los dias de vacaciones proporcionales
-        double diasProporcionales = ((getAñosDesdeAniversario2022() % 1) * diasVacas);
+        double diasProporcionales = (getAñosDesdeAniversario2022() * diasVacas);
 
         // regresa con 2 decimales
-        return Math.round((total + diasProporcionales) * 100.0) / 100.0;
+        return totalVacaciones + diasProporcionales;
 
     }
 
+    // Formula que calcula los dias de vacaciones usando la vieja ley
+    public double calcularUsandoViejaLey()    {
 
-    public double calcularVacacionesViejaLey()    {
-
-        double años = getAñosHastaAniversario2022();
+        double añosViejaLey = getAñosHastaAniversario2022();
         double totalVacaciones = 0;
-        int vacacionesPorAño = 6;
+        int vacacionesPorAño = 4;
+        int i;
 
-        for(int i = 1; i <= 5 && años > 0; i++, años--, vacacionesPorAño += 2)
+        for(i = 0; i < 4 && i < añosViejaLey ; i++){
+            vacacionesPorAño += 2;
             totalVacaciones += vacacionesPorAño;
+        }
 
-        vacacionesPorAño -= 2;
-
-        for (int i = 1; i <= 4 && años > 0; i++, años--)
-            totalVacaciones += vacacionesPorAño;
-
-        vacacionesPorAño += 2;
-
-        for (int i = 1; años > 0; i++, años--)
+        for (i = i + 1; i <= añosViejaLey; i++)
         {
-            totalVacaciones += vacacionesPorAño;
-
             if (i % 5 == 0)
                 vacacionesPorAño += 2;
+
+            totalVacaciones += vacacionesPorAño;
         }
         return totalVacaciones;
 
@@ -99,25 +85,41 @@ public class Empleado {
 
     // Obtiene años trabajados hasta el cambio de ley del 2022
     public double getAñosHastaAniversario2022() {
-        LocalDate aniversario = LocalDate.of(
-                2022,
-                fechaIngreso.getMonth(),
-                fechaIngreso.getDayOfMonth());
 
-        return Period.between(fechaIngreso, aniversario).getYears();
+        double periodo = Period.between(
+                fechaIngreso,
+                LocalDate.of(
+                        2022,
+                        fechaIngreso.getMonth(),
+                        fechaIngreso.getDayOfMonth())
+        ).getYears();
+
+        // Se agrega esta condifion porque el metodo between puede devolver numeros negativos
+        // y esto afecta el funcionamiento del resto del programa
+        return periodo > 0 ? periodo : 0;
     }
 
     // Obtiene los años trabajados desde el cambio de ley del 2022
+    // Este metodo se usa para calcular el proporcional desde el 2022,
+    // solo la parte fraccionaria, por eso no se toman en cuenta los años
     public double getAñosDesdeAniversario2022() {
-        LocalDate aniversario = LocalDate.of(
-                2022,
-                fechaIngreso.getMonth(),
-                fechaIngreso.getDayOfMonth());
 
-        Period period = Period.between(aniversario, LocalDate.now());
 
-        // Devuelve con numero decimal
-        return Math.round((period.getYears() + ((double)(period.getMonths() * 30 + period.getDays()) / 365)) * 100.0) / 100.0;
+        Period period = Period.between(
+                LocalDate.of(
+                        2022, // se toma el 2022 como refeerencia pero lo importante son el dia y mes de ingreso
+                        fechaIngreso.getMonth(),
+                        fechaIngreso.getDayOfMonth())
+                , LocalDate.now());
+
+        /* Se multiplican los meses por (365/12) debido a los decimales
+         si se dividiera 365 / 12 el resultado es 30.41666666... periodico, y al redondear se pierde un decimal
+         de modo que al año no se cumplan los dias de vacaciones completos
+         haciendolo de esta manera se respetan los decimales y se realiza el calculo mas precizo posible
+         */
+        double decimales = ((double)(period.getMonths() * (365/12) + period.getDays()) / 365);
+
+        return decimales;
     }
 
 }
--- src/test/java/SistemaTest.java
+++ src/test/java/SistemaTest.java
@@ -7,52 +7,122 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 import java.io.ByteArrayOutputStream;
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 
 
 public class SistemaTest {
 
-    Empleado empleado;
-    ByteArrayOutputStream out;
+    @Test
+    public void pruebaAñosViejaLey1año(){
+        Empleado e = new Empleado("Luis", LocalDate.of(2021, 1,1), 0);
+        Assert.assertEquals(1,e.getAñosHastaAniversario2022(), 0);
 
-    @Before
-    public void before(){
-        out = new ByteArrayOutputStream();
+        e = new Empleado("Luis", LocalDate.of(2021, 6,15), 0);
+        Assert.assertEquals(1,e.getAñosHastaAniversario2022(), 0);
+
+        e = new Empleado("Luis", LocalDate.of(2021, 12,31), 0);
+        Assert.assertEquals(1,e.getAñosHastaAniversario2022(), 0);
+    }
+
+    @Test
+    public void pruebaAñosViejaLeyMuchos(){
+        Empleado e = new Empleado("Luis", LocalDate.of(2020, 1,1), 0);
+        Assert.assertEquals(2,e.getAñosHastaAniversario2022(), 0);
+
+        e = new Empleado("Luis", LocalDate.of(2019, 6,15), 0);
+        Assert.assertEquals(3,e.getAñosHastaAniversario2022(), 0);
+
+        e = new Empleado("Luis", LocalDate.of(2018, 12,31), 0);
+        Assert.assertEquals(4,e.getAñosHastaAniversario2022(), 0);
+    }
+
+    @Test
+    public void pruebaCalcularVacacionesViejaLey1año(){
+        Empleado e = new Empleado("Luis", LocalDate.of(2021, 1,1), 0);
+        Assert.assertEquals(6,e.calcularUsandoViejaLey(), 0);
+
+        e = new Empleado("Luis", LocalDate.of(2021, 6,15), 0);
+        Assert.assertEquals(6,e.calcularUsandoViejaLey(), 0);
+
+        e = new Empleado("Luis", LocalDate.of(2021, 12,31), 0);
+        Assert.assertEquals(6,e.calcularUsandoViejaLey(), 0);
+    }
+    @Test
+    public void pruebaCalcularVacacionesViejaLeyMuchosaños(){
+        // 2 años
+        Empleado e = new Empleado("Luis", LocalDate.of(2020, 1,1), 0);
+        Assert.assertEquals(14,e.calcularUsandoViejaLey(), 0);
+
+        // 3 años
+        e = new Empleado("Luis", LocalDate.of(2019, 6,15), 0);
+        Assert.assertEquals(24,e.calcularUsandoViejaLey(), 0);
+
+        // 4 años
+        e = new Empleado("Luis", LocalDate.of(2018, 12,31), 0);
+        Assert.assertEquals(36,e.calcularUsandoViejaLey(), 0);
+
+        // 5 años
+        e = new Empleado("Luis", LocalDate.of(2017, 1,1), 0);
+        Assert.assertEquals(50,e.calcularUsandoViejaLey(), 0);
+
+        // 6 años
+        e = new Empleado("Luis", LocalDate.of(2016, 6,15), 0);
+        Assert.assertEquals(64,e.calcularUsandoViejaLey(), 0);
+
+        // 10 años
+        e = new Empleado("Luis", LocalDate.of(2012, 12,31), 0);
+        Assert.assertEquals(122,e.calcularUsandoViejaLey(), 0);
+
+        // 11 años
+        e = new Empleado("Luis", LocalDate.of(2011, 12,31), 0);
+        Assert.assertEquals(138,e.calcularUsandoViejaLey(), 0);
+
+        // 11 años
+        e = new Empleado("Luis", LocalDate.of(2022 - 43, 12,31), 0);
+        Assert.assertEquals(848,e.calcularUsandoViejaLey(), 0);
+
     }
 
 
     @Test
-    public void añosViejaLey(){
-        empleado = new Empleado("Luis", LocalDate.of(1992,8,13), 0);
-        Assert.assertEquals(30, empleado.getAñosHastaAniversario2022(),0);
+    public void pruebaCalcularUsandoNuevaLey(){
+
+        // 1 mes
+        Empleado e = new Empleado("Luis", LocalDate.of(2023, 2,10), 0);
+        Assert.assertEquals(1, Double.parseDouble(new DecimalFormat("#.#").format(e.calcularVacaciones())), 0);
+
+        // 2 meses
+        e = new Empleado("Luis", LocalDate.of(2023, 1,10), 0);
+        Assert.assertEquals(2, Double.parseDouble(new DecimalFormat("#.#").format(e.calcularVacaciones())), 0);
+
+        // 5 meses
+        e = new Empleado("Luis", LocalDate.of(2022, 10,10), 0);
+        Assert.assertEquals(5, Double.parseDouble(new DecimalFormat("#.#").format(e.calcularVacaciones())), 0);
+
+        // 12 meses
+        e = new Empleado("Luis", LocalDate.of(2022, 3,10), 0);
+        Assert.assertEquals(12, Double.parseDouble(new DecimalFormat("#.#").format(e.calcularVacaciones())), 0);
+
+        // 12 meses y medio
+        e = new Empleado("Luis", LocalDate.of(2021, 9,10), 0);
+        Assert.assertEquals(12.9, Double.parseDouble(new DecimalFormat("#.#").format(e.calcularVacaciones())), 0);
+
     }
 
     @Test
-    public void añosNuevaLey(){
-        empleado = new Empleado("Luis", LocalDate.of(1992,8,13), 0);
-        Assert.assertEquals(0.55, empleado.getAñosDesdeAniversario2022(),0);
+    public void vacacionesFinales(){
+        // 1 año
+        Empleado e = new Empleado("Luis", LocalDate.of(2023 - 1, 3,10), 0);
+        Assert.assertEquals(12.0, Double.parseDouble(new DecimalFormat("#.#").format(e.calcularVacaciones())), 0);
+
+        // 2 mes
+        e = new Empleado("Luis", LocalDate.of(2023- 2 , 3,10), 0);
+        Assert.assertEquals(20.0, Double.parseDouble(new DecimalFormat("#.#").format(e.calcularVacaciones())), 0);
+
+        // 2 mes
+        e = new Empleado("Luis", LocalDate.of(2023 - 5, 3,10), 0);
+        Assert.assertEquals(56.1, Double.parseDouble(new DecimalFormat("#.#").format(e.calcularVacaciones())), 0);
     }
-
-    @Test
-    public void totalDeDias_Luis(){
-        empleado = new Empleado("Luis", LocalDate.of(1992,8,13), 0);
-        Assert.assertEquals(527.6 , empleado.calcularVacacionesProporcionalesNUEVAley(), 0);
-    }
-
-    @Test
-    public void totalDeDias_Araceli(){
-        empleado = new Empleado("Arroyo Aldana Araceli", LocalDate.of(2010,7,16), 141);
-        Assert.assertEquals(168.88 , empleado.calcularVacacionesProporcionalesNUEVAley(), 0);
-    }
-
-    @Test
-    public void totalDeDias_ElPepe(){
-        empleado = new Empleado("El pepe", LocalDate.of(2022,3,5), 141);
-        Assert.assertEquals(12 , empleado.calcularVacacionesProporcionalesNUEVAley(), 0);
-    }
-
-
-
-
 
 }
